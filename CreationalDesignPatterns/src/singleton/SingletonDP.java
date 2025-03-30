571b941699b22adf8686008f08040d75d577a97a
shrishail madari <shrishailmadari@gmail.com>
2025-03-30 16:56:53 +0530

    add these files

---
+++ CreationalDesignPatterns/src/singleton/SingletonDP.java
@@ -0,0 +1,24 @@
+package singleton;
+class PrimMinister{
+    private static  String pm = null;
+    private PrimMinister (){
+
+    }
+    public static String factoryMethod(){
+        if (pm==null){
+            pm = "new prime minister got Elected :❤️❤️❤️❤️❤️❤️";
+            System.out.println("New PM,,,😍😍😍");
+        }
+        else System.out.println("PM is Elected Only Once ");
+        return pm;
+    }
+}
+public class SingletonDP {
+
+    public static void main(String[] args) {
+        String string = PrimMinister.factoryMethod();
+        System.out.println(string);
+        PrimMinister.factoryMethod();
+    }
+
+}
